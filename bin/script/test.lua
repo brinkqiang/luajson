e699fa0f0c4126275c2e2db079f7efff097346a5
brinkqiang <brink.qiang@gmail.com>
2022-05-12 11:07:12 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -8,6 +8,9 @@ local foo = {
 	test3 = 1434.23988823,
 	test4 = -12345678987654321,
 	test5 = -1434.23988823,
+	test6 = 652898023277872107,
+	test7 = 652898023277872108,
+	
 }
 
 local str = json.encode(foo)
@@ -16,3 +19,7 @@ print(str)
 local foo2 = json.decode(str)
 local str2 = json.encode(foo2)
 print(str2)
+
+print(foo.test7 - foo.test6)
+
+print(foo.test6 - foo.test7)
